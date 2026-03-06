cb21e7db7bfb934fd4fce4166e190eeab1342130
dci-student <aleksandra.adamchik@gmail.com>
2026-03-06 07:36:11 +0100

    feat(storefront): implement voting flow and migrate to generic class-based views

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+import debug_toolbar
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
+
+
+# Debug toolbar only in dev
+urlpatterns = [
+    path("__debug__/", include(debug_toolbar.urls)),
+] + urlpatterns
